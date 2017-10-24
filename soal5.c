c517c589f9628ec5a50d1fef0d83f2e79303d454
natval98 <natasha16@mhs.if.its.ac.id>
2017-10-24 22:55:32 +0700

    revisi fungsi soal5 krn tidak ada newline dalam novel.txt jadi pakai naivesearch

--- soal5.c
+++ soal5.c
@@ -1,32 +1,46 @@
 #include <stdio.h>
 #include <string.h>
+#include <pthread.h>
 
-
-
-int carinama(char cari[])
+void* carinama(char cari[])
 {
 	int banyaknama = 0;
+	
 	FILE *fileIn;
 	char *line = NULL;
 	size_t len = 0;
 	fileIn = fopen ("Novel.txt","r");
-	while (getline (&line, &len, fileIn) != -1){
-		if (strstr(line,cari)!=NULL){
-			banyaknama++;
-		}
+	char txt [2000];	//karena sudah tau jumlahnya
+
+	fgets (txt, sizeof(txt), fileIn);
+	int pjgnama = strlen (cari);
+	int pjgtxt = strlen (txt);
+	int banyak = 0;
+//	printf("pgjnama %d pgxt %d\n",pjgnama,pjgtxt);
+
+	for (int i = 0; i <= pjgtxt - pjgnama; i++)
+    {
+        int j;
+  
+        /* For current index i, check for pattern match */
+        for (j = 0; j < pjgnama; j++)
+            if (txt[i+j] != cari[j])
+                break;
+ 
+        if (j == pjgnama){  // if pat[0...M-1] = txt[i, i+1, ...i+M-1]
+//           printf("Pattern found at index %d n", i);
+		banyak++;
 	}
-	return banyaknama;
+    }
+	printf("BANYAK %d\n",banyak);
 }
 
 int main()
 {
-	char nama [100];
-	scanf("%s",nama);
-
-	int banyak;
-	banyak = carinama(nama);
-	printf("%d\n",banyak);
+	char nama [1000];
+	while (scanf("%s",nama)!=EOF)
+	{
+		carinama (nama);
+	}
 	return 0;
 }
-
-		

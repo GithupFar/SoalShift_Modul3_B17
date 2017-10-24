3cf7f7b0584cd2c9e504591fdbd52ebad5e928bd
natval98 <natasha16@mhs.if.its.ac.id>
2017-10-25 01:05:26 +0700

    membuat fungsi thread soal3 untuk mengurangi makan

---
+++ soal3.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+pthread_t tid[3];//inisialisasi array untuk menampung thread dalam kasusu ini ada 2 thread
+
+int lohstat = 100, kepistat = 100;
+void* kasihmakan(void *arg)
+{
+	unsigned long i=0;
+    pthread_t id=pthread_self();
+    int iter;
+    if(pthread_equal(id,tid[0]))//thread untuk counter time lohan
+    {
+		while (lohstat <=100 && lohstat >=0)
+		{
+			printf("Berkurang 10 lho!");
+			sleep (1);
+			lohstat -= 10;
+			
+		}
+	}
+/*	
+	printf("ini ada yang jalan lho");
+	system ("cvlc bagimu-negri.mp3");
+*/	
+	else if (pthread_equal (id,tid[1]))
+	{
+		while (kepistat <=100 && kepistat >=0)
+		{
+			printf("Berkurang 10 kepitingnya lho!");
+			sleep (2);
+			kepistat -= 10;
+		}
+	}
+    else if(pthread_equal(id,tid[2]))
+    {
+		int anu;
+        scanf("%d",&anu);
+		lohstat += anu;
+		system ("pkill");
+    }
+    return NULL;
+}
+int main(void)
+{
+    int i=0;
+    int err;
+    while(i<2)//looping membuat thread 2x
+    {
+        err=pthread_create(&(tid[i]),NULL,&kasihmakan,NULL);//membuat thread
+        if(err!=0)//cek error
+        {
+            printf("\n can't create thread : [%s]",strerror(err));
+        }
+        else
+        {
+            printf("\n create thread success");
+        }
+		pthread_join(tid[i],NULL);
+        i++;
+    }
+    return 0;
+}

a88ec4a3b5c72a9b711bb0a99c4fabb7fb293673
natval98 <natasha16@mhs.if.its.ac.id>
2017-10-24 21:27:53 +0700

    membuat soal4 hingga EOF dan maksimal faktorial adalah 20

--- soal4.c
+++ soal4.c
@@ -1,23 +1,25 @@
 #include<stdio.h>
 #include<pthread.h>
+typedef long long int LL;
 
 int i,j;
 void factorial(int a)
 {
-    int hasil=1;
+    LL hasil=1;
     for(i=1;i<=a;i++){
         hasil=i*hasil;
     }
-    printf("Hasil %d! = %d\n",a,hasil);
+    printf("Hasil %d! = %lld\n",a,hasil);		
+		//faktorial yg masih bisa dihitung dengan LL adalah hingga 20
 }
 
 int main(){
     int n=0,t;
-    pthread_t tid[n];
-    while(1){
-        scanf("%d",&t);
+    pthread_t tid[100];
+    while(scanf("%d",&t)!=EOF){		//membuat hingga EOF
         pthread_create(&tid[n],NULL,factorial,t);
         pthread_join(tid[n], NULL);
         n++;
     }
+	return 0;
 }

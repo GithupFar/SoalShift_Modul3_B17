625d47446d34d954646f342486c55df8bff53e04
natval98 <natasha16@mhs.if.its.ac.id>
2017-10-25 00:22:26 +0700

    main tebakranjau blm thread

--- soal2.c
+++ soal2.c
@@ -16,6 +16,7 @@ void isiranjau(int num)
 	}
 	
 	mine [i][j] = 1; //diisi ranjau
+	minetot++;
 }
 
 void taruhranjau(){
@@ -28,9 +29,45 @@ void taruhranjau(){
 	}
 }
 
+void tebakranjau(int player){
+	int i,j ,score = 0, ans;
+	for (int x=1;x<=4;x++) //harus nebak sebanyak 4 kali
+	{
+		scanf("%d",&ans);
+		if (ans % 4 == 0){
+			i = (ans/4)-1;
+			j = (ans%4)-1;
+		}
+		else{
+			i = ans/4;
+			j = ans%4;
+		}
+	
+		if (mine[i][j]){	//kalau kena ranjau, lawannya +poin
+			printf("DUAR KENA RANJAU");
+			if (player==1) p2++;
+			else if (player==2) p1++;
+		}
+	}
+}
+
 int main()
 {
 	memset (mine,0,sizeof (mine));
-	
+	p1 = 0; p2 = 0;
+	while (1)
+	{
+		printf("p1 taruh\n");
+		taruhranjau();
+		printf("p2 tebak\n");
+		tebakranjau(2);
+		if (p1>10 || p2>10 || minetot>=16) break;		
+		
+		printf("p2 taruh\n");
+		taruhranjau();
+		printf("p1 tebak\n");
+		tebakranjau(1);
+		if (p1>10 || p2>10 || minetot>=16) break;		
+	}
 	return 0;
 }

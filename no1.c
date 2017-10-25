c9a134bf5e4bb48d715d673601f0fac7d7d0e081
Unknown <magzksky@gmail.com>
2017-10-25 10:24:07 +0700

    basic format

---
+++ no1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int adminadd ()
+{
+
+
+}
+
+int admincount ()
+{
+
+
+
+}
+
+int customeradd()
+{
+
+
+
+}
+
+int customercount ()
+{
+
+
+
+}
+
+
+int main ()
+{
+
+
+
+}

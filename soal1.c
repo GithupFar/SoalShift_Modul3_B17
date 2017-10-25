5d5b8a92f6829a7f3ef46e4892da00be61daf091
Unknown <magzksky@gmail.com>
2017-10-25 15:02:05 +0700

    stage2

--- soal1.c
+++ soal1.c
@@ -5,7 +5,7 @@
 
 typedef struct
 {
-char *name;
+char name[10];
 int qty;
 }detail;
 
@@ -20,7 +20,33 @@ int search(detail *no1,detail *no2)
 int main ()
 {
 	detail stock[] ={{"MP4A1",0},{"PM2-V1",0},{"SPR-3",0},{"SS2-V5",0},{"SPG1-V3",0},{"MINE",0}};
+	detail input;
+	int a,b;
+	while (1)
+{
 	printf("---Welcome to Khawari Shop---\n");
+	printf("Choose your account\n");
+	printf("1.Admin\n");
+	printf("2.costumer\n");
+	scanf ("%d",&a);
+	if (a==1)//  data admin
+	{
 
-}
-
+	printf ("\n\n--Menu--\n");
+	printf ("1. tambah Stock\n");
+	printf ("2. lihat stock\n");
+	scanf ("%d",&b);
+		if (b==1)//input data
+		{
+		scanf("%[^\n] %d", input.name, input.qty);
+		printf("%s %d", input.name, input.qty);
+		}
+	}
+	else if (a==2)// data pelanggan
+	{
+	printf ("--Welcome--\n");
+        printf ("1. Beli Senjata\n");
+        printf ("2. lihat stock\n");
+	}
+};
+)

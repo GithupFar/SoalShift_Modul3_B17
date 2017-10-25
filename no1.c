4e9ec267549c16fd55c6bf842bbf72ea83396969
Unknown <magzksky@gmail.com>
2017-10-25 11:24:15 +0700

    CHANGE PLAN

--- no1.c
+++ no1.c
@@ -1,40 +1,23 @@
 #include <stdio.h>
-#include <iostream>
 #include <unistd.h>
 
-using namespace std;
-
-int adminadd ()
+struct details
 {
+const char* name[10];
+int qty;
+};
 
-
-}
-
-int admincount ()
-{
-
-
-
-}
-
-int customeradd()
-{
-
-
-
-}
-
-int customercount ()
-{
-
-
-
-}
+struct details stock[10];
 
 
 int main ()
 {
-
-
+	*stock[1].name="MP4A1";
+        *stock[2].name="PM2-V1";
+        *stock[3].name="SPR-3";
+        *stock[4].name="MP4A1";
+        *stock[5].name="SS2-V5";
+        *stock[6].name="SPG1-V3";
+        *stock[7].name="MINE";
 
 }

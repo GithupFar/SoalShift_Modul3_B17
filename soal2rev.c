e7330719fc0148114b1cf712bf81ea1efd73fbcf
natval98 <natasha16@mhs.if.its.ac.id>
2017-11-01 23:31:42 +0700

    soal2 tambah nama

--- soal2rev.c
+++ soal2rev.c
@@ -7,6 +7,7 @@
 pthread_t tid [2];
 int mine [5][5]; //4x4 >> mulai dari 0,1,2,3
 int minetot,p1,p2;
+char namap1[50], namap2[50];
 pthread_mutex_t lock;
 
 void isiranjau(int num)
@@ -43,11 +44,11 @@ void* play1 (void* arg)
 {
 	int ranjau,pasang,tebak;
 	pthread_mutex_lock (&lock);
-	printf("p1 memasang ranjau!\n");
+	printf("p1 (%s) memasang ranjau!\n",namap1);
 	
 	while (1)
 	{
-		printf("Berapa banyak ranjau yg mau dipasang oleh p1? ");
+		printf("Berapa banyak ranjau yg mau dipasang oleh p1 (%s)? ",namap1);
 		scanf("%d",&ranjau);
 		if (ranjau > 4) printf("input invalid! Masukkan 1-4 saja!\n");
 		else if (ranjau >= 0 && ranjau <= 4) break;
@@ -61,16 +62,16 @@ void* play1 (void* arg)
 		isiranjau (pasang);
 		printf("Ranjau sudah dipasang di lubang %d\n",pasang);
 	}
-	printf("\np2 menebak ranjau sebanyak 4 kali!\n");
+	printf("\np2 (%s) menebak ranjau sebanyak 4 kali!\n",namap2);
 	for (int i=0;i<4;i++){
 		printf("Mau tebak lubang mana saja? ");
 		scanf("%d",&tebak);
 		int temp = cekranjau (tebak);
-		if (temp) printf("p2 Kena RANJAU!\n");
+		if (temp) printf("p2 (%s) Kena RANJAU!\n",namap2);
 		p1 += temp;
 	}
 
-	printf("\n\nStatus => p1 : %d \t p2 : %d\n\n",p1,p2);
+	printf("\n\nStatus => p1 (%s) : %d \t p2 (%s) : %d\n\n",namap1,p1,namap2,p2);
 	pthread_mutex_unlock (&lock);
 }
 
@@ -78,11 +79,11 @@ void* play2 (void* arg)
 {
 	int ranjau,pasang,tebak;
 	pthread_mutex_lock (&lock);
-	printf("p2 memasang ranjau!\n");
+	printf("p2 (%s) memasang ranjau!\n",namap2);
 	
 	while (1)
 	{
-		printf("Berapa banyak ranjau yg mau dipasang oleh p2? ");
+		printf("Berapa banyak ranjau yg mau dipasang oleh p2 (%s)? ",namap2);
 		scanf("%d",&ranjau);
 		if (ranjau > 4) printf("input invalid! Masukkan 1-4 saja!\n");
 		else if (ranjau >= 0 && ranjau <= 4) break;
@@ -97,16 +98,16 @@ void* play2 (void* arg)
 		isiranjau (pasang);
 		printf("Ranjau sudah dipasang di lubang %d\n",pasang);
 	}
-	printf("p1 menebak ranjau sebanyak 4 kali!\n");
+	printf("p1 (%s) menebak ranjau sebanyak 4 kali!\n",namap1);
 	for (int i=0;i<4;i++){
 		printf("Mau tebak lubang mana saja? ");
 		scanf("%d",&tebak);
 		int temp = cekranjau (tebak);
-		if (temp) printf("p1 Kena RANJAU!\n");
+		if (temp) printf("p1 (%s) Kena RANJAU!\n",namap1);
 		p2 += temp;
 	}
 
-	printf("\n\nStatus => p1 : %d \t p2 : %d\n\n",p1,p2);
+	printf("\n\nStatus => p1 (%s) : %d \t p2 (%s) : %d\n\n",namap1,p1,namap2,p2);
 	pthread_mutex_unlock (&lock);
 }
 
@@ -115,6 +116,12 @@ int main()
 	minetot = 0; p1 = 0; p2 = 0;
 
 	printf("LET'S PLAY!\n");
+	
+	printf("Masukkan nama pemain 1 : ");
+	scanf("%s",namap1);
+	printf("Masukkan nama pemain 2 : ");
+	scanf("%s",namap2);
+
 	while (1)
 	{
 		if (minetot>15 || p1 > 9 || p2 > 9){
@@ -135,8 +142,9 @@ int main()
 
 	pthread_mutex_destroy (&lock);
 	printf("Winner of this Game!\n");
-	if (p1 > p2) printf("\n=====\np1 win\n=====\n");
-	else if (p2 > p1) printf("\n=====\np2 win\n=====\n");
+	printf("\n\nSkor Akhir => p1 (%s) : %d \t p2 (%s) : %d\n\n",namap1,p1,namap2,p2);
+	if (p1 > p2) printf("\n=====\np1(%s) win\n=====\n",namap1);
+	else if (p2 > p1) printf("\n=====\np2 (%s) win\n=====\n",namap2);
 	else printf("No one win\n");
 	return 0;
 }
